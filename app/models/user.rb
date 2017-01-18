0bf07adfdc2c2eedb2028cf5ade4d9756a692c45
wwinslowkent <wwinslowkent@gmail.com>
2017-01-18 15:54:53 -0500

    remove user confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,6 @@ class User < ApplicationRecord
   validates :name, uniqueness: { message: "that username is already in use" }
   validates :name, presence: true
   devise :database_authenticatable, :registerable,
-         :recoverable, :confirmable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable
 
-  def confirmation_required?
-    !confirmed?
-  end
 end
