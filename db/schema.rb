a8a3111536ba93dc39af18ba61a862f5d9ba0ded
wwinslowkent <wwinslowkent@gmail.com>
2017-01-18 11:51:14 -0500

    remove mailer

--- db/schema.rb
+++ db/schema.rb
@@ -25,13 +25,8 @@ ActiveRecord::Schema.define(version: 20170117203554) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["name"], name: "index_users_on_name", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
