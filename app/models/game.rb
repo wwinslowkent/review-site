8c6d396229319dcce45ed26ca4798440d9ab7b65
Josh Ho <hojo1025@gmail.com>
2017-01-19 15:50:19 -0500

    add review controller, add review view, add tests for review

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,5 +2,5 @@ class Game < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :summary, presence: true
   validates :cover_url, presence: true
-
+  has_many :reviews
 end
