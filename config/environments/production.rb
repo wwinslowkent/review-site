62bcb1d9bb2b4f06aad415ae0bd1e33b395adacb
vscalzo000 <vscalzo000@gmail.com>
2017-01-25 15:04:50 -0500

    Adds mailer info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,11 +87,11 @@ Rails.application.configure do
   #Mailgun setup for heroku
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'galaxy-gamers.herokuapp.com',
+    :address => ‘smtp.sendgrid.net’,
+    :port => ‘587’,
     :authentication => :plain,
+    :user_name => ENV[‘SENDGRID_USERNAME’],
+    :password => ENV[‘SENDGRID_PASSWORD’],
+    :domain => 'galaxy-gamers.herokuapp.com'
   }
 end
